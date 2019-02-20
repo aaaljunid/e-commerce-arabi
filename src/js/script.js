692b14266e65fd70b69e167dc5aa9f21fdf3c490
aaaljunid <junaidprofee@gmail.com>
2019-02-20 11:42:33 +0300

    add new items in the page of index.html and style.css

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('[data-add-to-cart]').click(function(e){
+        alert('أضيف المنتج الى العربة');
+        e.stopPropagation();
+    });
 });
