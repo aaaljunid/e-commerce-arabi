61cb2355d56ee454ae16ff346f69084ceb58006a
aaaljunid <junaidprofee@gmail.com>
2019-02-24 13:34:59 +0300

    add data-add-to-cart for all

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,7 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 
     $('[data-add-to-cart]').click(function(e){
-<<<<<<< HEAD
         alert('تم اضافة المنتج الى العربة');
         e.stopPropagation();
-    });
-   
-=======
-        alert('أضيف المنتج الى العربة');
-        e.stopPropagation();
-    });
->>>>>>> 692b14266e65fd70b69e167dc5aa9f21fdf3c490
+
 });
