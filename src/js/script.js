bc7478bd6f16188478bb7f3900d39f8328488854
aaaljunid <junaidprofee@gmail.com>
2019-02-24 12:50:21 +0300

    add data-add-to-cart to all button

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
 
     $('[data-toggle="tooltip"]').tooltip();
 
+    $('[data-add-to-cart]').click(function(e){
+        alert('تم اضافة المنتج الى العربة');
+        e.stopPropagation();
+    });
+   
 });
