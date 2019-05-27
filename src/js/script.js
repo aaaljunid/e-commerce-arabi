f5093cefcde527f5d29db4fb0f757d5a6e003a21
aaaljunid <30616133+aaaljunid@users.noreply.github.com>
2019-05-27 12:29:44 +0300

    add checkout page with problem in width 991

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,57 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 
     $('[data-add-to-cart]').click(function(e){
-        alert('تم اضافة المنتج الى العربة');
+        alert("تم اضافة المنتج الى العربة");
         e.stopPropagation();
     });
 
-    $('.product-option input[type="radio"]').change(function() {
-        $(this).parents('.product-option').siblings().removeClass('active');
-        $(this).parents('.product-option').addClass('active');
+
+
+    $(".product-option input[type='radio']").change(function(){
+        $(this).parents(".product-option").siblings().removeClass('active');
+        $(this).parents(".product-option").addClass('active');
     });
 
+
+
+    $('[data-remove-from-cart]').click(function(){
+        $(this).parents('[data-product-info]').remove();
+
+        calculateTotalPrice();
+    });
+
+
+
+
+    $('[data-product-quantity]').change(function(){
+
+        var $newQuantity = $(this).val();
+
+        var $parent = $(this).parents('[data-product-info]');
+
+        var $price = $parent.attr('data-product-price');
+
+        var $total = $price * $newQuantity;
+
+        $parent.find('.total-price-for-product').text($total + '$');
+
+        calculateTotalPrice();
+    });
+
+    function calculateTotalPrice(){
+        var totalPriceForAll = 0;
+
+        $('[data-product-info]').each(function(){
+            
+            var price = $(this).attr('data-product-price');
+
+            var quantity = $(this).find('[data-product-quantity]').val();
+
+            var totalPriceForProduct = price * quantity;
+
+            totalPriceForAll = totalPriceForAll + totalPriceForProduct;
+        });
+
+        $("#total-price-for-all").text(totalPriceForAll + "$");
+    }
 });
